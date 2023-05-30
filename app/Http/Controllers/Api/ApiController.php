29685e792e13c2bc1fda06bc057a2ec5167d5ff5
Jair Quinto <tokinmym2@gmail.com>
2023-05-30 09:02:09 -0500

    30-05-23 - api Citys

--- app/Http/Controllers/Api/ApiController.php
+++ app/Http/Controllers/Api/ApiController.php
@@ -61,12 +61,12 @@ class ApiController extends Controller
 	public function city()
 	{
 		$city = new City;
-		$text = new Text;
-		$lid =  isset($_GET['lid']) && $_GET['lid'] > 0 ? $_GET['lid'] : 0;
+		//$text = new Text;
+		//$lid =  isset($_GET['lid']) && $_GET['lid'] > 0 ? $_GET['lid'] : 0;
 
 		return response()->json([
 			'data' => $city->getAll(0),
-			'text' => $text->getAppData($lid)
+			//'text' => $text->getAppData($lid)
 		]);
 	}
 
