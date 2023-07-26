e0d190a86f259cdbd3885aa6e0c37d8122725e4a
Jair Quinto <tokinmym2@gmail.com>
2023-07-26 14:02:05 -0500

    data

--- app/Models/User.php
+++ app/Models/User.php
@@ -229,7 +229,7 @@ class User extends Authenticatable
 
             // Obtenemos las categorias
             if ($cat == 0 ) {
-                $get_c = CategoryStore::where('type_cat', 2)->pluck('id')->toArray();
+                $get_c = CategoryStore::pluck('id')->toArray();
             } else {
                 $get_c = CategoryStore::where('type_cat', 2)->where('id_c', $cat)->pluck('id')->toArray();
             }
@@ -238,8 +238,6 @@ class User extends Authenticatable
             
 
             $query->whereIn('users.subsubtype', $get_c);
-
-
 
 
         })->select('users.*', DB::raw("6371 * acos(cos(radians(" . $lat . ")) 
