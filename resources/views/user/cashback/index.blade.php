ab16c0f9095b092c68a1d2084f27c2f7e26a5c7d
Enel Villafranca <enel.villafranca@tigears.com>
2024-03-12 20:37:24 -0400

    24 horas

--- resources/views/user/cashback/index.blade.php
+++ resources/views/user/cashback/index.blade.php
@@ -38,18 +38,17 @@
                                 @foreach ($blocked_days as $day)
                                     <div class="mb-3 mx-2" style="border: solid 2px; width: 180px; padding: 10px;">
                                         <h4>{{ Carbon\Carbon::parse($day->fecha)->format('d-m-Y') }}
-                                        <a href="javascript:void()" class="btn btn-danger" onclick="deleteConfirm('{{ Asset($link . 'delete/' . $day->id) }}')">
-                                            <i class="mdi mdi-delete-forever"></i>
-                                        </a>
-                                    </h4>
+                                            <a href="javascript:void()" class="btn btn-danger"
+                                                onclick="deleteConfirm('{{ Asset($link . 'delete/' . $day->id) }}')">
+                                                <i class="mdi mdi-delete-forever"></i>
+                                            </a>
+                                        </h4>
                                     </div>
                                 @endforeach
                             </div>
+                        </div>
                     </div>
-                </div>
 
-                <div class="row">
-                    <div class="col-lg-12 mx-auto mt-2">
                         <div class="card py-3 m-b-30">
 
                             <div class="row">
@@ -60,13 +59,16 @@
                                         href="{{ Asset($link . 'add') }}"
                                         class="btn btn-warning rounded-pill waves-effect waves-light width-md">
                                         Agregar Nuevo</a>&nbsp;
-                                        <button type="button" class="btn btn-success rounded-pill width-md waves-effect waves-light mr-2" onclick="handleImport()">Importar Excel</button>
+                                    <button type="button"
+                                        class="btn btn-success rounded-pill width-md waves-effect waves-light mr-2"
+                                        onclick="handleImport()">Importar Excel</button>
                                 </div>
 
                             </div>
 
 
                             <div class="card-body">
+                                <div class="overflow-scroll">
                                 <table class="table table-hover ">
                                     <thead>
                                         <tr>
@@ -95,30 +97,28 @@
                                         @endforeach
                                     </tbody>
                                 </table>
+                                </div>
                             </div>
                         </div>
 
-                    </div>
                 </div>
             </div>
         </div>
-    </div>
 
-    <form action="{{ route('cashback.import') }}" method="POST" id="import"
-        enctype="multipart/form-data">
-        @csrf
-        <input type="file" name="import_file" class="d-none" onchange="submitImport()">
-    </form>
-@endsection
-@section('js')
-    <script>
-        function handleImport() {
-            $('input[name=import_file]').click();
-        }
+        <form action="{{ route('cashback.import') }}" method="POST" id="import" enctype="multipart/form-data">
+            @csrf
+            <input type="file" name="import_file" class="d-none" onchange="submitImport()">
+        </form>
+    @endsection
+    @section('js')
+        <script>
+            function handleImport() {
+                $('input[name=import_file]').click();
+            }
 
-        // Importar porcentajes de horas
-        function submitImport() {
-            $('#import').submit();
-        }
-    </script>
-@endsection
+            // Importar porcentajes de horas
+            function submitImport() {
+                $('#import').submit();
+            }
+        </script>
+    @endsection
